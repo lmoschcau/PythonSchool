6db489c73ca90efe38850adb1f7cb1ec80964897
Leonard Moschcau <l.moschcau@web.de>
2018-03-10 22:22:57 +0100

    merge

--- Sorting/Sorting.py
+++ Sorting/Sorting.py
@@ -11,6 +11,8 @@ colorama.init()
 AnzahlAufrufe = 0
 AnzahlAbfragen = 0
 
+SpaltenBreite = 16
+
 cMax = 1000
 def vertausche(Feld, i, j):
     Feld[i], Feld[j] = Feld[j], Feld [i]
@@ -82,12 +84,12 @@ def FuelleZufall(ArrayLaenge, ZufallMin, ZufallMax):
         Feld.append(random.randint(ZufallMin, ZufallMax))
     return Feld
 
-def Eingabe(Nachricht, Fehlermeldung):
+def Eingabe(Nachricht, typ, Fehlermeldung):
     print(colorama.Fore.GREEN + Nachricht + colorama.Fore.CYAN)
     Eingabe = None
     while (Eingabe == None):
         try:
-            Eingabe = int(input(">"))
+            Eingabe = typ(input(">"))
         except ValueError:
             print(colorama.Fore.RED + Fehlermeldung + colorama.Style.RESET_ALL)
     return Eingabe
@@ -96,28 +98,36 @@ Fehler = "Bitte Geben Sie eine ganze natürliche Zahl an."
 
 EingabeFeld = None
 
-ArrayLaenge = Eingabe("Bitte die Länge des Feldes angeben:", Fehler)
+ArrayLaenge = Eingabe("Bitte die Länge des Feldes angeben:", int, Fehler)
 while (EingabeFeld == None):
-    ZufallMin = Eingabe("Bitte den niedrigsten Wert angeben:", Fehler)
-    ZufallMax = Eingabe("Bitte den höchsten Wert angeben:", Fehler)
+    ZufallMin = Eingabe("Bitte den niedrigsten Wert angeben:", int, Fehler)
+    ZufallMax = Eingabe("Bitte den höchsten Wert angeben:", int, Fehler)
     try:
         EingabeFeld = FuelleZufall(ArrayLaenge, ZufallMin, ZufallMax)
     except ValueError:
-        print("\033[2J\033[1;1f" + colorama.Fore.RED + "Fehler: niedrigster Wert (" + str(ZufallMin) + ") ist größer höchster Wert (" + str(ZufallMax) + ")" + colorama.Style.RESET_ALL)
+        print("\033[2J\033[1;1f" + colorama.Fore.RED + "Fehler: niedrigster Wert (" + str(ZufallMin) + ") ist größer höchster Wert (" + str(ZufallMax) + ")!" + colorama.Style.RESET_ALL)
 
 Zeiten = [None] * 4
 Aufrufe = [None] * 4
 Abfragen = [None] * 4
+Funktionen = [None] * 4
 
-def Visualisierung(Feld):
+def VisualisierungVertikal(Feld):
     for i in range(max(Feld), min(Feld) - 1, -1):
         horizontal = ' '
         for j in range(len(Feld)):
             if Feld[j] >= i:
-                horizontal += '#'
+                horizontal += '█'
             else:
                 horizontal += ' '
-        print horizontal
+        print(horizontal)
+
+def VisualisierungHorizontal(Feld):
+    for i in range(len(Feld)):
+        horizontal = '■' * Feld[i]
+        print(horizontal)
+def VisualisierungFeld(Feld):
+    print (colorama.Fore.BLUE + str(EingabeFeld) + colorama.Style.RESET_ALL)
 
 def RufeAuf(Algorithmus, AufrufFeld, i):
     global AnzahlAufrufe
@@ -129,25 +139,51 @@ def RufeAuf(Algorithmus, AufrufFeld, i):
     Zeiten[i] = datetime.datetime.now() - StartZeit
     Aufrufe[i] = AnzahlAufrufe
     Abfragen[i] = AnzahlAbfragen
-    print '\n'
+    Funktionen[i] = Algorithmus.__name__
+    print ('\n')
     Visualisierung(sortiertesFeld)
+    #Visualisierung(sortiertesFeld)
     print("AnzahlAufrufe: " + str(AnzahlAufrufe) + " AnzahlAbfragen: " + str(AnzahlAbfragen) + " Zeit: " + str(Zeiten[i]))
 
     return sortiertesFeld
+Visualisierung = None
 
-    
-print (colorama.Fore.BLUE + str(EingabeFeld) + colorama.Style.RESET_ALL)
+while (Visualisierung == None):
+    Visualisierungen = {"v":VisualisierungVertikal, "h":VisualisierungHorizontal, "f":VisualisierungFeld}
+    NutzerEingabe = Eingabe("Visualisierung bitte auswählen: Vertikal[V] Horizontal[H] Feld[F]", str, "Bitte einen Eintrag aus der auswahl angeben.")
+    NutzerEingabe = NutzerEingabe.lower()[:1]
+    try:
+        Visualisierung = Visualisierungen[NutzerEingabe]
+    except KeyError:
+        print("\033[2J\033[1;1f" + colorama.Fore.RED + "Fehler: Die Eingabe (" + str(NutzerEingabe) + ") ist keine gültige Auswahl!" + colorama.Style.RESET_ALL)
 
-print '\nunsortiertes Feld:\n'
+print ('\033[2J\033[1;1funsortiertes Feld:\n')
 Visualisierung(list(EingabeFeld))
 
-print '\nsortiertes Feld (Auswahl):'
-RufeAuf(AuswahlSort, list(EingabeFeld), 0)
-print '\nsortiertes Feld (Einfügen):'
-RufeAuf(EinSort, list(EingabeFeld), 1)
-print '\nsortiertes Feld (Bubble-Sort):'
+print ('\nsortiertes Feld (Auswahl):')
+res1 = RufeAuf(AuswahlSort, list(EingabeFeld), 0)
+print ('\nsortiertes Feld (Einfügen):')
+res2 = RufeAuf(EinSort, list(EingabeFeld), 1)
+print ('\nsortiertes Feld (Bubble-Sort):')
 RufeAuf(BubbleSort, list(EingabeFeld), 2)
-print '\nsortiertes Feld (Bubble+):'
+print ('\nsortiertes Feld (Bubble+):')
 RufeAuf(BubbleSort2, list(EingabeFeld), 3)
-result = AuswahlSort( list(EingabeFeld))
-        
+
+
+print ('\nStatistiken:\n')
+
+print(colorama.Fore.LIGHTGREEN_EX + colorama.Back.LIGHTBLACK_EX + "Das Sortieren von einem Feld mit " + str(ArrayLaenge) + " Einträgen war erfolgreich." + colorama.Fore.GREEN + colorama.Back.RESET)
+
+def GebeTabellenZeileAus(Name, Feld):
+    AusgabeZeile = (str(Name) + "                                  ")[:SpaltenBreite]
+    for i in range(len(Feld)):
+        AusgabeZelle = str(Feld[i]) + "                                  "
+        AusgabeZeile += AusgabeZelle[:SpaltenBreite]
+    print(AusgabeZeile)
+
+GebeTabellenZeileAus("Algorithmus", Funktionen)
+GebeTabellenZeileAus("Aufrufe", Aufrufe)
+GebeTabellenZeileAus("Abfragen", Abfragen)
+GebeTabellenZeileAus("Zeiten", Zeiten)
+
+
