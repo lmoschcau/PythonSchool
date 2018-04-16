1385f062a8baa13ac5c174c1c7335174182397c8
Myzhe <Utini8m@gmail.com>
2018-04-16 20:50:02 +0200

    added binary search

--- hvh/util/search.py
+++ hvh/util/search.py
@@ -10,3 +10,19 @@ def sequ2(Feld, key):
     while Feld[i] != key:
         i += 1
     return (i != len(Feld) - 1)
+
+def bin(Feld, key):
+    return binFunktion(Feld, 0, len(Feld), key)
+
+def binFunktion(Feld, erstes, letztes, key):
+    if (letztes < erstes):
+        return False
+    else:
+        mitte = (erstes + letztes) / 2;
+        if (key == Feld[mitte]):
+            return True
+        else:
+            if (key < Feld[mitte]):
+                return binFunktion(Feld, erstes, mitte - 1, key)
+            else:
+                return binFunktion(Feld, mitte + 1, letztes, key)
